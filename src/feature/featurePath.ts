bd8868f75e77392b87421ed420fcc5b913734cdd
pipopotamasu <ec0156hx@gmail.com>
2025-01-14 10:06:21 -0500

    feat(feature): 'platform' feature type is a new one to enable a variety of features FS app has
    'platform' feature type is an experimental feature type that enables users to access a variety of
    features FS app currently has.

--- src/feature/featurePath.ts
+++ src/feature/featurePath.ts
@@ -1,7 +1,8 @@
-type FeaturePaths = "/treatment_plans/new";
+type FeaturePaths = "/treatment_plans/new" | "/o/home";
 
 const FEATURE_PATHS: { [key: string]: FeaturePaths } = {
   treatmentPlan: "/treatment_plans/new",
+  platform: "/o/home",
 };
 
 export { FeaturePaths, FEATURE_PATHS };
